f5c7e6c225a51dccc2bbccebac461124ffb71e4a
Pingjiang Wang <pinwang@microsoft.com>
2020-06-22 13:34:49 +0000

    workflow serivce - azure implementation.

---
+++ provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/repository/IngestionStrategyRepository.java
@@ -0,0 +1,83 @@
+//  Copyright © Microsoft Corporation
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package org.opengroup.osdu.workflow.provider.azure.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.opengroup.osdu.azure.CosmosFacade;
+import org.opengroup.osdu.core.common.model.WorkflowType;
+import org.opengroup.osdu.workflow.model.IngestionStrategy;
+import org.opengroup.osdu.workflow.provider.azure.model.IngestionStrategyDoc;
+import org.opengroup.osdu.workflow.provider.interfaces.IIngestionStrategyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.azure.cosmos.CosmosContainer;
+
+import javax.inject.Named;
+
+@Repository
+@Slf4j
+@RequiredArgsConstructor
+public class IngestionStrategyRepository implements IIngestionStrategyRepository {
+
+  private static Logger logger = Logger.getLogger(IngestionStrategyRepository.class.getName());
+
+  @Autowired
+  @Named("INGESTION_STRATEGY_CONTAINER")
+  private CosmosContainer ingestionStrategyContainer;
+
+  @Override
+  public IngestionStrategy findByWorkflowTypeAndDataTypeAndUserId(WorkflowType workflowType,
+                                                                  String dataType, String userId) {
+    logger.log(Level.INFO, String.format("Requesting dag selection. Workflow type: {%s}, Data type: {%s}, User id: {%s}",
+      workflowType, dataType, userId));
+
+    Optional<IngestionStrategyDoc> document = CosmosFacade.findItem(
+      ingestionStrategyContainer,
+      String.format("%s-%s", workflowType.toString().toLowerCase(), dataType.toLowerCase()),
+      workflowType.toString().toLowerCase(),
+      IngestionStrategyDoc.class);
+
+    IngestionStrategyDoc ingestionStrategyDoc = !document.isPresent() ? null : document.get();
+
+    IngestionStrategy ingestionStrategy = null;
+
+    if(ingestionStrategyDoc != null) {
+      logger.log(Level.INFO, String.format("Found dag: {%s}", ingestionStrategyDoc.dagName));
+      ingestionStrategy = buildIngestionStrategy(ingestionStrategyDoc);
+    }
+
+    return ingestionStrategy;
+  }
+
+  private IngestionStrategy buildIngestionStrategy(IngestionStrategyDoc ingestionStrategyDoc) {
+    logger.log(Level.INFO, String.format("Build ingestion strategy. Ingestion Strategy Doc: {%s}",
+      ingestionStrategyDoc.toString()));
+
+    return IngestionStrategy.builder()
+      .workflowType(WorkflowType.valueOf(ingestionStrategyDoc.workflowType.toUpperCase()))
+      .dataType(ingestionStrategyDoc.dataType)
+      .userId(ingestionStrategyDoc.userId)
+      .dagName(ingestionStrategyDoc.dagName)
+      .build();
+  }
+}
