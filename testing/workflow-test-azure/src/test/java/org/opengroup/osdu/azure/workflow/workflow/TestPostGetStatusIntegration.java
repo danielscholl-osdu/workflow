59100b036034ee262e2676fa6bceea7fc12488f5
satish.nargani <satish.nargani@wipro.com>
2020-08-20 14:27:05 +0000

    removed sysout

--- testing/workflow-test-azure/src/test/java/org/opengroup/osdu/azure/workflow/workflow/TestPostGetStatusIntegration.java
+++ testing/workflow-test-azure/src/test/java/org/opengroup/osdu/azure/workflow/workflow/TestPostGetStatusIntegration.java
@@ -35,7 +35,6 @@ public class TestPostGetStatusIntegration extends PostGetStatusIntegrationTests 
   @Test
   @Override
   public void should_returnUnauthorized_when_notGivenAccessToken()  {
-    System.out.println("Inside Tests");
     ClientResponse response = client.send(
         HttpMethod.POST,
         GET_STATUS_URL,
