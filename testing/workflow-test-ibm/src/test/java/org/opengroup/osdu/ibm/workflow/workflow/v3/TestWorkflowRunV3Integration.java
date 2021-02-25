96ab5d96164869607bab5b02d57793e40e793d73
Bhushan Rade <bhusrad1@in.ibm.com>
2021-02-25 05:51:55 -0500

    IBM impl for getAllWorkflowsForTenant api and ibm int test

--- testing/workflow-test-ibm/src/test/java/org/opengroup/osdu/ibm/workflow/workflow/v3/TestWorkflowRunV3Integration.java
+++ testing/workflow-test-ibm/src/test/java/org/opengroup/osdu/ibm/workflow/workflow/v3/TestWorkflowRunV3Integration.java
@@ -1,6 +1,13 @@
 
 package org.opengroup.osdu.ibm.workflow.workflow.v3;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.opengroup.osdu.workflow.consts.TestConstants.GET_DETAILS_WORKFLOW_RUN_URL;
+
+import java.util.UUID;
+
+import javax.ws.rs.HttpMethod;
+
 import org.junit.Ignore;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +15,9 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.opengroup.osdu.ibm.workflow.util.HTTPClientIBM;
 import org.opengroup.osdu.workflow.workflow.v3.WorkflowRunV3IntegrationTests;
+import org.springframework.http.HttpStatus;
+
+import com.sun.jersey.api.client.ClientResponse;
 
 public class TestWorkflowRunV3Integration extends WorkflowRunV3IntegrationTests {
 
@@ -20,42 +30,31 @@ public class TestWorkflowRunV3Integration extends WorkflowRunV3IntegrationTests 
 
 	@AfterEach
 	@Override
-	public void tearDown() {
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(30000);
 		deleteAllTestWorkflowRecords();
 		this.client = null;
 		this.headers = null;
 	}
 
-	@Override
-	@Test
-	@Disabled
-	public void shouldReturn200WhenTriggerNewWorkflow() throws Exception {
-		// TODO Auto-generated method stub
-		super.shouldReturn200WhenTriggerNewWorkflow();
-	}
-
-	@Override
-	@Test
-	@Disabled
-	public void shouldReturn200WhenGetAllRunInstances() throws Exception {
-		// TODO Auto-generated method stub
-		super.shouldReturn200WhenGetAllRunInstances();
-	}
 
 	@Override
 	@Test
 	@Disabled
 	public void shouldReturn400WhenGetDetailsForSpecificWorkflowRunInstance() throws Exception {
-		// TODO Auto-generated method stub
-		super.shouldReturn400WhenGetDetailsForSpecificWorkflowRunInstance();
-	}
+		String workflowId = UUID
+		        .randomUUID().toString().replace("-", "");
+		    String runId = UUID
+		        .randomUUID().toString().replace("-", "");
 
-	@Override
-	@Test
-	@Disabled
-	public void shouldReturn200WhenUpdateWorkflowRunInstance() throws Exception {
-		// TODO Auto-generated method stub
-		super.shouldReturn200WhenUpdateWorkflowRunInstance();
+		    ClientResponse getResponse = client.send(
+		        HttpMethod.GET,
+		        String.format(GET_DETAILS_WORKFLOW_RUN_URL, workflowId, runId),
+		        null,
+		        headers,
+		        client.getAccessToken()
+		    );
+		    assertEquals(HttpStatus.NOT_FOUND, getResponse.getStatus());
 	}
 
 	private void deleteAllTestWorkflowRecords() {
