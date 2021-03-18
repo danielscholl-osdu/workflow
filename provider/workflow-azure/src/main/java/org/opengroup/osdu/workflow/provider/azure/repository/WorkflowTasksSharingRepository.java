ca6ff1632a58161fc0a04802b861631fa5374aec
aalekhj <aaljain@microsoft.com>
2021-03-18 17:24:04 +0530

    Updated `WorkflowTasksSharingRepository.java`

--- provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/repository/WorkflowTasksSharingRepository.java
+++ provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/repository/WorkflowTasksSharingRepository.java
@@ -4,13 +4,15 @@ import com.azure.storage.blob.sas.BlobContainerSasPermission;
 import org.opengroup.osdu.azure.blobstorage.BlobStore;
 import org.opengroup.osdu.azure.cosmosdb.CosmosStore;
 import org.opengroup.osdu.core.common.model.http.DpsHeaders;
-import org.opengroup.osdu.workflow.provider.azure.config.BlobConfig;
 import org.opengroup.osdu.workflow.provider.azure.config.CosmosConfig;
-import org.opengroup.osdu.workflow.provider.azure.interfaces.IWorkflowTasksSharingRepository;
+import org.opengroup.osdu.workflow.provider.azure.model.WorkflowTasksSharingDoc;
+import org.opengroup.osdu.workflow.provider.azure.utils.WorkflowTasksSharingUtils;
+import org.opengroup.osdu.workflow.provider.interfaces.IWorkflowTasksSharingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -20,7 +22,7 @@ public class WorkflowTasksSharingRepository implements IWorkflowTasksSharingRepo
   BlobStore blobStore;
 
   @Autowired
-  DpsHeaders headers;
+  DpsHeaders dpsHeaders;
 
   @Autowired
   CosmosStore cosmosStore;
@@ -29,35 +31,46 @@ public class WorkflowTasksSharingRepository implements IWorkflowTasksSharingRepo
   CosmosConfig cosmosConfig;
 
   @Autowired
-  BlobConfig blobConfig;
+  WorkflowTasksSharingUtils workflowTasksSharingUtils;
 
   @Override
-  public String getSignedUrl(String workflowId, String runId) {
-    final String dataPartitionId = headers.getPartitionId();
-    final String uuid = UUID.randomUUID().toString();
-    final String filePath = String.format("%s/%s/%s", workflowId, runId, uuid);
+  public String getSignedUrl(String workflowName, String runId) {
+    final String dataPartitionId = dpsHeaders.getPartitionId();
     // TODO : Add support for using user provided expiry time and permissions (?)
-    int expiryDays = 7;
+    final int expiryDays = 7;
     final OffsetDateTime expiryTime = OffsetDateTime.now().plusDays(expiryDays);
-    final BlobContainerSasPermission permissions =
-        new BlobContainerSasPermission().setCreatePermission(true).setReadPermission(true)
-            .setWritePermission(true).setListPermission(true);
-    final String signedUrl =
-        blobStore.generatePreSignedURL(dataPartitionId, blobConfig.getTasksSharingContainer(), expiryTime, permissions);
-//
-//    WorkflowTasksSharingDoc workflowTasksSharingDoc =
-//        WorkflowTasksSharingDoc.builder()
-//            .id(uuid)
-//            .workflowId(workflowId)
-//            .runId(runId)
-//            .filePath(filePath)
-//            .createdAt(System.currentTimeMillis())
-//            .createdBy(headers.getUserEmail())
-//            .build();
-//
-//    cosmosStore.createItem(headers.getPartitionId(), cosmosConfig.getDatabase(),
-//        cosmosConfig.getWorkflowTasksSharingCollection(), workflowTasksSharingDoc.getWorkflowId(), workflowTasksSharingDoc);
+    // TODO : Add support for custom permission (?)
+    final BlobContainerSasPermission permissions = new BlobContainerSasPermission()
+        .setCreatePermission(true)
+        .setReadPermission(true)
+        .setWritePermission(true)
+        .setListPermission(true);
 
-    return signedUrl;
+    final List<WorkflowTasksSharingDoc> workflowTasksSharingDocs = workflowTasksSharingUtils.getWorkflowTasksSharingDocsForWorkflowNameAndRunId(dataPartitionId, workflowName, runId);
+
+    String containerId;
+
+    if (!workflowTasksSharingDocs.isEmpty()) {
+      containerId = workflowTasksSharingDocs.get(0).getId();
+    } else {
+      containerId = UUID.randomUUID().toString();
+      blobStore.createBlobContainer(dataPartitionId, containerId);
+      WorkflowTasksSharingDoc workflowTasksSharingDocNewContainer = workflowTasksSharingDocBuilder(workflowName, runId, containerId);
+      cosmosStore.createItem(dataPartitionId, cosmosConfig.getDatabase(),
+          cosmosConfig.getWorkflowTasksSharingCollection(), workflowTasksSharingDocNewContainer.getPartitionKey(), workflowTasksSharingDocNewContainer);
+    }
+    return blobStore.generatePreSignedURL(dataPartitionId, containerId, expiryTime, permissions);
+  }
+
+  WorkflowTasksSharingDoc workflowTasksSharingDocBuilder(String workflowName, String runId, String containerId) {
+    return WorkflowTasksSharingDoc.builder()
+        .id(UUID.randomUUID().toString())
+        .partitionKey(workflowName)
+        .containerId(containerId)
+        .workflowName(workflowName)
+        .runId(runId)
+        .createdAt(System.currentTimeMillis())
+        .createdBy(dpsHeaders.getUserEmail())
+        .build();
   }
 }
