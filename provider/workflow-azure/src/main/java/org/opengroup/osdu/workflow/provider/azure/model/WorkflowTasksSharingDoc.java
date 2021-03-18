92d5e72ab5ecb8e685510c9864bb0ca94c68789d
aalekhj <aaljain@microsoft.com>
2021-03-18 17:16:34 +0530

    Updated `WorkflowTasksSharingDoc.java`

--- provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/model/WorkflowTasksSharingDoc.java
+++ provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/model/WorkflowTasksSharingDoc.java
@@ -1,6 +1,8 @@
 package org.opengroup.osdu.workflow.provider.azure.model;
 
+import com.azure.spring.data.cosmos.core.mapping.GeneratedValue;
 import lombok.*;
+import org.springframework.data.annotation.Id;
 
 @Builder
 @Getter
@@ -9,9 +11,10 @@ import lombok.*;
 @EqualsAndHashCode
 public class WorkflowTasksSharingDoc {
   private String id;
-  private String workflowId;
+  private String partitionKey;
   private String runId;
-  private String filePath;
+  private String workflowName;
+  private String containerId;
   private Long createdAt;
   private String createdBy;
 }
