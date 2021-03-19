89ed4bc79c8f4611fe5df5fc15151d9c2d1bef47
aalekhj <aaljain@microsoft.com>
2021-03-19 14:18:48 +0530

    Updated `WorkflowTasksSharingUtils.java`
    Removed method `getWorkflowTasksSharingDocsForWorkflowNameAndRunId`

--- provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/utils/WorkflowTasksSharingUtils.java
+++ provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/utils/WorkflowTasksSharingUtils.java
@@ -1,17 +1,14 @@
 package org.opengroup.osdu.workflow.provider.azure.utils;
 
-import com.azure.cosmos.models.SqlParameter;
-import com.azure.cosmos.models.SqlQuerySpec;
 import org.opengroup.osdu.azure.blobstorage.BlobStore;
 import org.opengroup.osdu.azure.cosmosdb.CosmosStore;
 import org.opengroup.osdu.core.common.logging.JaxRsDpsLog;
-import org.opengroup.osdu.workflow.exception.WorkflowRunNotFoundException;
 import org.opengroup.osdu.workflow.provider.azure.config.CosmosConfig;
 import org.opengroup.osdu.workflow.provider.azure.model.WorkflowTasksSharingDoc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.Optional;
 
 @Component
 public class WorkflowTasksSharingUtils {
@@ -29,29 +26,18 @@ public class WorkflowTasksSharingUtils {
     @Autowired
     private JaxRsDpsLog logger;
 
-    public List<WorkflowTasksSharingDoc> getWorkflowTasksSharingDocsForWorkflowNameAndRunId(String dataPartitionId, String workflowName, String runId) {
-        SqlParameter workflowNameParameter = new SqlParameter("@workflowName", workflowName);
-        SqlParameter runIdParameter = new SqlParameter("@runId", runId);
-        SqlQuerySpec sqlQuerySpec = new SqlQuerySpec(
-            "SELECT * from c where c.workflowName = @workflowName and c.runId = @runId ORDER BY c._ts DESC",
-            workflowNameParameter, runIdParameter);
-        return cosmosStore.queryItems(dataPartitionId, cosmosConfig.getDatabase(),
-            cosmosConfig.getWorkflowTasksSharingCollection(), sqlQuerySpec, null, WorkflowTasksSharingDoc.class);
-    }
-
     public void deleteTasksSharingInfoContainer(String dataPartitionId, String workflowName, String runId) {
-        final List<WorkflowTasksSharingDoc> workflowTasksSharingDocs =
-            getWorkflowTasksSharingDocsForWorkflowNameAndRunId(dataPartitionId, workflowName, runId);
+        final Optional<WorkflowTasksSharingDoc> optionalWorkflowTasksSharingDoc =
+            cosmosStore.findItem(dataPartitionId, cosmosConfig.getDatabase(), cosmosConfig.getWorkflowTasksSharingCollection(), runId, workflowName, WorkflowTasksSharingDoc.class);
 
-        if (!workflowTasksSharingDocs.isEmpty()) {
-            String id = workflowTasksSharingDocs.get(0).getId();
-            String containerId = workflowTasksSharingDocs.get(0).getContainerId();
+        if (optionalWorkflowTasksSharingDoc.isPresent()) {
+          String containerId = optionalWorkflowTasksSharingDoc.get().getContainerId();
             blobStore.deleteBlobContainer(dataPartitionId, containerId);
             cosmosStore.deleteItem(
                 dataPartitionId,
                 cosmosConfig.getDatabase(),
                 cosmosConfig.getWorkflowTasksSharingCollection(),
-                id,
+                runId,
                 workflowName);
         } else {
             // throw error (do we require a separate exception class for this?)
