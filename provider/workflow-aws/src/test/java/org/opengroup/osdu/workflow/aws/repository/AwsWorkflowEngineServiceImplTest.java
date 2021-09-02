32250157ec6be45a1703cd70082c98f6cd1ab2fd
Spencer Sutton <suttonsp@amazon.com>
2021-09-02 14:12:26 +0000

    Airflow AWS S3 Refactor
    commit 2f6800bc 
    Author: Spencer Sutton <suttonsp@amazon.com> 
    Date: Wed Sep 01 2021 20:22:18 GMT-0500 (Central Daylight Time) 
    
        Clean up
    
    
    commit 14a30328 
    Author: Spencer Sutton <suttonsp@amazon.com> 
    Date: Wed Sep 01 2021 16:12:30 GMT-0500 (Central Daylight Time) 
    
        Writing unit tests
    
    
    commit c435b1b6 
    Author: Spencer Sutton <suttonsp@amazon.com> 
    Date: Wed Sep 01 2021 10:29:05 GMT-0500 (Central Daylight Time) 
    
        Updating s3 saving
    
    
    commit 69b0ffa6 
    Author: Spencer Sutton <suttonsp@amazon.com> 
    Date: Fri Aug 27 2021 12:29:12 GMT-0500 (Central Daylight Time) 
    
        Adding s3 client for aws

---
+++ provider/workflow-aws/src/test/java/org/opengroup/osdu/workflow/aws/repository/AwsWorkflowEngineServiceImplTest.java
@@ -0,0 +1,130 @@
+package org.opengroup.osdu.workflow.aws.repository;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.jersey.api.client.Client;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.internal.configuration.injection.MockInjection;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opengroup.osdu.core.aws.s3.IS3ClientFactory;
+import org.opengroup.osdu.core.aws.s3.S3ClientWithBucket;
+import org.opengroup.osdu.core.common.model.http.DpsHeaders;
+import org.opengroup.osdu.workflow.aws.service.AwsWorkflowEngineServiceImpl;
+import org.opengroup.osdu.workflow.aws.service.airflow.sqs.WorkflowRequestBodyFactory;
+import org.opengroup.osdu.workflow.aws.service.airflow.sqs.WorkflowSqsClient;
+import org.opengroup.osdu.workflow.aws.service.s3.S3Client;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.opengroup.osdu.core.aws.dynamodb.DynamoDBQueryHelper;
+import org.opengroup.osdu.core.common.model.http.AppException;
+import org.opengroup.osdu.workflow.aws.config.AwsAirflowApiMode;
+import org.opengroup.osdu.workflow.aws.config.AwsServiceConfig;
+import org.opengroup.osdu.workflow.aws.repository.AwsWorkflowRunRepository;
+import org.opengroup.osdu.workflow.aws.service.s3.S3Client;
+import org.opengroup.osdu.workflow.aws.util.dynamodb.converters.WorkflowRunDoc;
+import org.opengroup.osdu.workflow.config.AirflowConfig;
+import org.opengroup.osdu.workflow.model.TriggerWorkflowResponse;
+import org.opengroup.osdu.workflow.model.WorkflowEngineRequest;
+import org.opengroup.osdu.workflow.model.WorkflowStatusType;
+import org.opengroup.osdu.workflow.provider.interfaces.IWorkflowEngineService;
+
+import javax.inject.Inject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest(classes={S3Client.class})
+public class AwsWorkflowEngineServiceImplTest {
+    @InjectMocks
+    AwsWorkflowEngineServiceImpl CUT = new AwsWorkflowEngineServiceImpl();
+
+    @Mock
+    private AwsServiceConfig config;
+
+    @Mock
+    private DpsHeaders dpsHeaders;
+
+    @Mock
+    AwsServiceConfig awsConfig;
+
+    @Mock
+    private AirflowConfig airflowConfig;
+
+    @Mock
+    private Client restClient;
+
+    @Mock
+    DpsHeaders headers;
+
+    @Mock
+    WorkflowSqsClient sqsClient;
+
+    @Mock
+    S3Client s3Client;
+
+    @Mock
+    private WorkflowRequestBodyFactory workflowRequestBodyFactory;
+
+    @Mock
+    AwsWorkflowRunRepository awsWorkflowRunRepository;
+
+    @Before
+    public void setUp() {
+      initMocks(this);
+    }
+
+    @Test
+    public void save()
+    {
+        // Arrange
+        String runId = "test-run-id";
+        String dagName = "test-dag-name";
+        String serializedData = "{some-serialized-data}";
+        String partitionId = "test-partition";
+        String ref = "test-ref";
+
+        WorkflowEngineRequest request = new WorkflowEngineRequest(runId, "workflowId", dagName, 1l,
+            dagName, "date", false);
+
+        Map<String, Object> inputData = new HashMap<>();
+
+        // to mock
+        Mockito.when(awsWorkflowRunRepository.runExists(Mockito.eq(runId)))
+            .thenReturn(false);
+
+        Mockito.when(awsConfig.getAirflowApiMode())
+            .thenReturn(AwsAirflowApiMode.SQS);
+
+        Mockito.when(workflowRequestBodyFactory.getSerializedWorkflowRequest(Mockito.anyMap(), Mockito.eq(dagName),
+            Mockito.eq(runId), Mockito.any(DpsHeaders.class), Mockito.anyBoolean()))
+            .thenReturn(serializedData);
+
+        Mockito.when(dpsHeaders.getPartitionId())
+            .thenReturn(partitionId);
+
+        Mockito.when(s3Client.save(Mockito.eq(runId), Mockito.eq(serializedData), Mockito.eq(partitionId)))
+            .thenReturn(ref);
+
+        Mockito.doNothing().when(sqsClient).sendMessageToWorkflowQueue(Mockito.eq(ref));
+
+        // Act
+        TriggerWorkflowResponse response = CUT.triggerWorkflow(request, inputData);
+
+        // Assert
+        Mockito.verify(awsWorkflowRunRepository, Mockito.times(1)).runExists(Mockito.eq(runId));
+
+        Mockito.verify(workflowRequestBodyFactory, Mockito.times(1)).getSerializedWorkflowRequest(Mockito.anyMap(), Mockito.eq(dagName),
+            Mockito.eq(runId), Mockito.any(DpsHeaders.class), Mockito.anyBoolean());
+
+        Mockito.verify(s3Client, Mockito.times(1)).save(Mockito.eq(runId), Mockito.eq(serializedData), Mockito.eq(partitionId));
+
+        Mockito.verify(sqsClient, Mockito.times(1)).sendMessageToWorkflowQueue(Mockito.eq(ref));
+    }
+}
