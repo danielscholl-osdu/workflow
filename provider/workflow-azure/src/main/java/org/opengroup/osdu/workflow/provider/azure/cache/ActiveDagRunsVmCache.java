0eda880cf9b2552c58c50c0ff5293a815805a7a3
aalekhj <aaljain@microsoft.com>
2021-11-14 03:25:31 +0530

    Added VmCache for tracking active dag runs

---
+++ provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/cache/ActiveDagRunsVmCache.java
@@ -0,0 +1,14 @@
+package org.opengroup.osdu.workflow.provider.azure.cache;
+
+import org.opengroup.osdu.core.common.cache.VmCache;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+@Component("ActiveDagRunsCache")
+@ConditionalOnProperty(value = "runtime.env.local", havingValue = "true")
+public class ActiveDagRunsVmCache extends VmCache<String, Integer> {
+  public ActiveDagRunsVmCache() {
+    super(20, 1000);
+    System.out.println("########################### USING LOCAL CACHE ###########################");
+  }
+}
