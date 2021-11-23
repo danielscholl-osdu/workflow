ede0f2b4a17ed1bf0864f2003434a48e16882515
aalekhj <aaljain@microsoft.com>
2021-11-24 00:11:20 +0530

    Updated `ActiveDagRunsRedisCache.java` to use Qualifier

--- provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/cache/ActiveDagRunsRedisCache.java
+++ provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/cache/ActiveDagRunsRedisCache.java
@@ -1,21 +1,18 @@
 package org.opengroup.osdu.workflow.provider.azure.cache;
 
 import org.opengroup.osdu.core.common.cache.RedisCache;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
-
-import javax.inject.Named;
 
 @Component("ActiveDagRunsCache")
 @ConditionalOnProperty(value = "runtime.env.local", havingValue = "false", matchIfMissing = true)
 public class ActiveDagRunsRedisCache extends RedisCache<String, Integer> {
 
   public ActiveDagRunsRedisCache(
-      final @Named("REDIS_HOST") String host,
-      final @Named("REDIS_PORT") int port,
-      final @Named("REDIS_PASSWORD") String password,
-      @Value("${redis.database}") final int database) {
-    super(host, port, password, 20, database, String.class, Integer.class);
+      final @Qualifier("REDIS_HOST") String host,
+      final @Qualifier("REDIS_PORT") int port,
+      final @Qualifier("REDIS_PASSWORD") String password) {
+    super(host, port, password, 20, String.class, Integer.class);
   }
 }
