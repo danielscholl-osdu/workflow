cd567f05a597d63d1101ad45d8843bfb710c22c3
aalekhj <aaljain@microsoft.com>
2021-11-24 00:33:30 +0530

    Updated `WorkflowMetadataRedisCache.java`

--- provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/cache/WorkflowMetadataRedisCache.java
+++ provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/cache/WorkflowMetadataRedisCache.java
@@ -2,21 +2,17 @@ package org.opengroup.osdu.workflow.provider.azure.cache;
 
 import org.opengroup.osdu.core.common.cache.RedisCache;
 import org.opengroup.osdu.workflow.model.WorkflowMetadata;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
-
-import javax.inject.Named;
 
 @Component("WorkflowMetadataCache")
 @ConditionalOnProperty(value = "runtime.env.local", havingValue = "false", matchIfMissing = true)
 public class WorkflowMetadataRedisCache extends RedisCache<String, WorkflowMetadata> {
   public WorkflowMetadataRedisCache(
-      final @Named("REDIS_HOST") String host,
-      final @Named("REDIS_PORT") int port,
-      final @Named("REDIS_PASSWORD") String password,
-      final @Named("CURSOR_REDIS_TTL") int timeout,
-      @Value("${redis.database}") final int database) {
-    super(host, port, password, timeout, database, String.class, WorkflowMetadata.class);
+      final @Qualifier("REDIS_HOST") String host,
+      final @Qualifier("REDIS_PORT") int port,
+      final @Qualifier("REDIS_PASSWORD") String password) {
+    super(host, port, password, 600, String.class, WorkflowMetadata.class);
   }
 }
