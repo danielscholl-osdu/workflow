fce09ae9e95662be48c1de494ad135b469dd001e
aalekhj <aaljain@microsoft.com>
2021-11-24 00:23:44 +0530

    Added Redis config

---
+++ provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package org.opengroup.osdu.workflow.provider.azure.config;
+
+import com.azure.security.keyvault.secrets.SecretClient;
+import org.opengroup.osdu.azure.KeyVaultFacade;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+
+  @Value("${redis.port:6380}")
+  public int redisPort;
+
+  @Bean
+  @Qualifier("REDIS_PORT")
+  public int getRedisPort() {
+    return redisPort;
+  }
+
+  @Bean
+  @Qualifier("REDIS_HOST")
+  public String redisHost(SecretClient kv) {
+    return KeyVaultFacade.getSecretWithValidation(kv, "redis-hostname");
+  }
+
+  @Bean
+  @Qualifier("REDIS_PASSWORD")
+  public String redisPassword(SecretClient kv) {
+    return KeyVaultFacade.getSecretWithValidation(kv, "redis-password");
+  }
+}
