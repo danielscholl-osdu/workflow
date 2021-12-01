66aa80f5e6008d05b60bc63cda49094b88f02c12
aalekhj <aaljain@microsoft.com>
2021-12-01 13:55:41 +0530

    Updaed `RedisConfig.java` to contain configuration properties

--- provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/config/RedisConfig.java
+++ provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/config/RedisConfig.java
@@ -1,42 +1,15 @@
 package org.opengroup.osdu.workflow.provider.azure.config;
 
-import com.azure.security.keyvault.secrets.SecretClient;
-import org.opengroup.osdu.azure.KeyVaultFacade;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties("osdu.azure.redis")
+@Getter
+@Setter
 public class RedisConfig {
-
-  @Value("${redis.port:6380}")
-  public int redisPort;
-
-  @Value("${redis.workflowMetadata.ttl:600}")
-  public int workflowMetadataTtl;
-
-  @Bean
-  @Qualifier("REDIS_PORT")
-  public int getRedisPort() {
-    return redisPort;
-  }
-
-  @Bean
-  @Qualifier("WORKFLOW_METADATA_REDIS_TTL")
-  public int getWorkflowMetadataTtl() {
-    return workflowMetadataTtl;
-  }
-
-  @Bean
-  @Qualifier("REDIS_HOST")
-  public String redisHost(SecretClient kv) {
-    return KeyVaultFacade.getSecretWithValidation(kv, "redis-hostname");
-  }
-
-  @Bean
-  @Qualifier("REDIS_PASSWORD")
-  public String redisPassword(SecretClient kv) {
-    return KeyVaultFacade.getSecretWithValidation(kv, "redis-password");
-  }
+  private int redisPort;
+  private int workflowMetadataTtl;
 }
