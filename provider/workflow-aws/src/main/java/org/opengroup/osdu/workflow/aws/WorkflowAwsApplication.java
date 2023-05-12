83096abed2650c0ad2dec86d272723993f2c3503
Yash Dholakia <ysdholak@amazon.com>
2023-05-11 21:03:42 -0700

    Fix Build and update version to 2.7.10

--- provider/workflow-aws/src/main/java/org/opengroup/osdu/workflow/aws/WorkflowAwsApplication.java
+++ provider/workflow-aws/src/main/java/org/opengroup/osdu/workflow/aws/WorkflowAwsApplication.java
@@ -19,7 +19,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan(
     basePackages = {"org.opengroup.osdu"},
     excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = MongoDBAutoconfigExclude.class)})
