936add6e4298ade99d820105fcf667c237085f55
Yash Dholakia <ysdholak@amazon.com>
2023-05-18 14:44:33 -0700

    Update Spring Cloud version to 2021.0.7 and remove Mvc

--- workflow-core/src/main/java/org/opengroup/osdu/workflow/WorkflowApplication.java
+++ workflow-core/src/main/java/org/opengroup/osdu/workflow/WorkflowApplication.java
@@ -18,9 +18,7 @@ package org.opengroup.osdu.workflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @SpringBootApplication(scanBasePackages = "org.opengroup")
 public class WorkflowApplication {
 
