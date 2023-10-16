d14a48fda57b6d828ca85000ec1a6ade4164ed40
saketh somaraju <saketh_somaraju@epam.com>
2023-10-16 04:28:27 +0000

    update test dummy dag

--- testing/workflow-test-azure/src/test/resources/dags/test_dummy_dag.py
+++ testing/workflow-test-azure/src/test/resources/dags/test_dummy_dag.py
@@ -1,4 +1,6 @@
 from airflow import DAG
+import time
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from airflow.operators.dummy_operator import DummyOperator
 
@@ -12,7 +14,15 @@ default_args = {
     'email_on_retry': False
 }
 
-dag = DAG('${dagId}', schedule_interval=None, default_args=default_args)
+
+
+dag = DAG('test_dummy_dag', schedule_interval=None, default_args=default_args)
+
+delay_python_task: PythonOperator = PythonOperator(task_id="delay_python_task",
+                                                   dag=dag,
+                                                   python_callable=lambda: time.sleep(20))
 
 # t1, t2, t3 and t4 are examples of tasks created using operators
-dummy_task = DummyOperator(task_id='dummy', dag=dag)
+dummy = DummyOperator(task_id='dummy', dag=dag)
+
+delay_python_task >> dummy
