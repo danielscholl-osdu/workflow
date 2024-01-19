176e22568a6204b3bb94f5af9c79b88440608d99
Anthony Ittiyera Vazhappilly <avazhappilly@microsoft.com>
2024-01-19 11:17:32 -0800

    create feature flag to toggle enableFullSwaggerUrlProperty

--- workflow-core/src/main/java/org/opengroup/osdu/workflow/swagger/SwaggerConfiguration.java
+++ workflow-core/src/main/java/org/opengroup/osdu/workflow/swagger/SwaggerConfiguration.java
@@ -1,49 +1,113 @@
 package org.opengroup.osdu.workflow.swagger;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.tags.Tag;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.opengroup.osdu.core.common.model.http.DpsHeaders;
 import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-@OpenAPIDefinition(
-    info = @Info(
-        title = "${api.title}",
-        description = "${api.description}",
-        version = "${api.version}",
-        contact = @Contact(name = "${api.contact.name}", email = "${api.contact.email}"),
-        license = @License(name = "${api.license.name}", url = "${api.license.url}")),
-    security = @SecurityRequirement(name = "Authorization"),
-    tags = {
-        @Tag(name = "workflow-run-api", description = "WorkflowRun related endpoints"),
-        @Tag(name = "workflow-manager-api", description = "WorkflowManager related endpoints"),
-        @Tag(name = "workflow-system-manager-api", description = "Workflow System Manager related endpoints"),
-        @Tag(name = "health", description = "Health related endpoints"),
-        @Tag(name = "info", description = "Version info endpoint")
-    }
-)
-@SecurityScheme(name = "Authorization", scheme = "bearer", bearerFormat = "Authorization", type = SecuritySchemeType.HTTP)
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
+@Profile("!noswagger")
 public class SwaggerConfiguration {
-  @Bean
-  public OperationCustomizer customize() {
-    return (operation, handlerMethod) -> {
-      Parameter dataPartitionId = new Parameter()
-          .name(DpsHeaders.DATA_PARTITION_ID)
-          .description("Tenant Id")
-          .in("header")
-          .required(true)
-          .schema(new StringSchema());
-      return operation.addParametersItem(dataPartitionId);
-    };
-  }
 
+    @Value("${api.title}")
+    private String apiTitle;
+
+    @Value("${api.description}")
+    private String apiDescription;
+
+    @Value("${api.version}")
+    private String apiVersion;
+
+    @Value("${api.contact.name}")
+    private String contactName;
+
+    @Value("${api.contact.email}")
+    private String contactEmail;
+
+    @Value("${api.license.name}")
+    private String licenseName;
+
+    @Value("${api.license.url}")
+    private String licenseUrl;
+
+    @Value("${api.server.url}")
+    private String apiServerUrl;
+
+    @Value("${api.server.fullUrl.enabled:false}")
+    private boolean isServerFullUrlEnabled;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("Authorization")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+        final String securitySchemeName = "Authorization";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securitySchemeName);
+        Components components = new Components().addSecuritySchemes(securitySchemeName, securityScheme);
+
+        OpenAPI openAPI = new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components)
+                .info(apiInfo())
+                .tags(tags());
+
+        if(isServerFullUrlEnabled)
+            return openAPI;
+        return openAPI
+                .servers(Arrays.asList(new Server().url(apiServerUrl)));
+    }
+
+    private List<Tag> tags() {
+        List<Tag> tags = new ArrayList<>();
+        tags.add(new Tag().name("workflow-run-api").description("WorkflowRun related endpoints"));
+        tags.add(new Tag().name("workflow-manager-api").description("WorkflowManager related endpoints"));
+        tags.add(new Tag().name("workflow-system-manager-api").description("Workflow System Manager related endpoints"));
+        tags.add(new Tag().name("health").description("Health related endpoints"));
+        tags.add(new Tag().name("info").description("Version info endpoint"));
+        return tags;
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title(apiTitle)
+                .description(apiDescription)
+                .version(apiVersion)
+                .license(new License().name(licenseName).url(licenseUrl))
+                .contact(new Contact().name(contactName).email(contactEmail));
+    }
+
+    @Bean
+    public OperationCustomizer operationCustomizer() {
+        return (operation, handlerMethod) -> {
+            Parameter dataPartitionId = new Parameter()
+                    .name(DpsHeaders.DATA_PARTITION_ID)
+                    .description("Tenant Id")
+                    .in("header")
+                    .required(true)
+                    .schema(new StringSchema());
+            return operation.addParametersItem(dataPartitionId);
+        };
+    }
 }
