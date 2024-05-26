6704d55c940137f30e7dfeba54bc02f3532891cb
IshaKumari <isha.kumari@ibm.com>
2024-05-26 14:37:57 +0530

    Spring 6 migration

--- provider/workflow-aws/src/main/java/org/opengroup/osdu/workflow/aws/repository/AwsWorkflowMetadataRepository.java
+++ provider/workflow-aws/src/main/java/org/opengroup/osdu/workflow/aws/repository/AwsWorkflowMetadataRepository.java
@@ -1,12 +1,12 @@
 /**
 * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-* 
+*
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
-* 
+*
 *      http://www.apache.org/licenses/LICENSE-2.0
-* 
+*
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,8 +22,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Inject;
 
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
