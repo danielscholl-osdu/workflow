932eacc525019cad2bb4cdec9e9e1faa98203a5f
IshaKumari <isha.kumari@ibm.com>
2024-06-04 19:22:18 +0530

    Revert testing Spring migration

--- testing/workflow-test-core/src/main/java/org/opengroup/osdu/workflow/workflow/v3/WorkflowRunV3IntegrationTests.java
+++ testing/workflow-test-core/src/main/java/org/opengroup/osdu/workflow/workflow/v3/WorkflowRunV3IntegrationTests.java
@@ -27,6 +27,7 @@ import org.junit.jupiter.api.condition.EnabledIf;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.opengroup.osdu.workflow.util.HTTPClient;
 import org.opengroup.osdu.workflow.util.v3.TestBase;
+import org.springframework.http.HttpStatus;
 
 import javax.ws.rs.HttpMethod;
 import java.util.ArrayList;
@@ -46,7 +47,7 @@ import static org.opengroup.osdu.workflow.util.PayloadBuilder.buildCreateWorkflo
 import static org.opengroup.osdu.workflow.util.PayloadBuilder.buildUpdateWorkflowRunInvalidPayloadStatus;
 import static org.opengroup.osdu.workflow.util.PayloadBuilder.buildUpdateWorkflowRunInvalidRequestPayload;
 import static org.opengroup.osdu.workflow.util.PayloadBuilder.buildUpdateWorkflowRunValidPayloadWithGivenStatus;
-import org.springframework.http.HttpStatus;
+
 public abstract class WorkflowRunV3IntegrationTests extends TestBase {
   private static final String INVALID_PREFIX = "backfill";
   private static final Integer INVALID_LIMIT = 1000;
@@ -199,11 +200,11 @@ public abstract class WorkflowRunV3IntegrationTests extends TestBase {
     createdWorkflowRuns.add(workflowRunInfo);
   }
 
-	/*
-	 * after switch to airflow 2.0 stable API, this has to be marked as ignore or
-	 * removed. Airflow 2.x stable API will always throw 409 Conflicts instead of 400
-	 * BAD REQUEST when you pass duplicate run id
-	 */
+  /*
+   * after switch to airflow 2.0 stable API, this has to be marked as ignore or
+   * removed. Airflow 2.x stable API will always throw 409 Conflicts instead of 400
+   * BAD REQUEST when you pass duplicate run id
+   */
   @Test
   public void triggerWorkflowRun_should_returnBadRequest_when_givenDuplicateRunId() throws  Exception {
     String workflowResponseBody = createWorkflow();
@@ -227,10 +228,10 @@ public abstract class WorkflowRunV3IntegrationTests extends TestBase {
     assertEquals(org.apache.http.HttpStatus.SC_BAD_REQUEST, duplicateRunIdResponse.getStatus());
   }
 
-	/*
-	 * Enable this Integration test for airflow 2.0 stable API by removing
-	 * '@Disbaled'
-	 */
+  /*
+   * Enable this Integration test for airflow 2.0 stable API by removing
+   * '@Disbaled'
+   */
   @Test
   @Disabled("Until switch to airflow 2.0 stable api")
   public void triggerWorkflowRun_should_returnConflict_when_givenDuplicateRunId_with_airflow2_stable_API() throws  Exception {
