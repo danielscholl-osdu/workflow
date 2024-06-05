68af1c459140c3a46c35e311c4f336a3e2b8f806
Vidyadharani Lokam <vidyadharani_lokam@epam.com>
2024-06-05 16:39:59 +0530

    update securityconfig in azure

--- provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/security/AzureIstioSecurityConfig.java
+++ provider/workflow-azure/src/main/java/org/opengroup/osdu/workflow/provider/azure/security/AzureIstioSecurityConfig.java
@@ -4,20 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import static org.opengroup.osdu.workflow.provider.azure.security.AadSecurityConfig.AUTH_ALLOWLIST;
-import static org.springframework.security.config.Customizer.withDefaults;
 
-
+@Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @ConditionalOnProperty(value = "azure.istio.auth.enabled", havingValue = "true", matchIfMissing = true)
