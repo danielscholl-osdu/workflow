1f6097f07dc58a7b251d256fe950fe2e78471742
IshaKumari <isha.kumari@ibm.com>
2024-08-16 16:30:03 +0530

    add configuration  for IBM

---
+++ provider/workflow-ibm/src/main/java/org/opengroup/osdu/workflow/provider/ibm/config/WebConfiguration.java
@@ -0,0 +1,15 @@
+package org.opengroup.osdu.workflow.provider.ibm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+  @Override
+  public void configurePathMatch(PathMatchConfigurer configure) {
+    configure.setUseTrailingSlashMatch(true);
+  }
+
+}
+
